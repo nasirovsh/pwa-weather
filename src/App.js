76b169577a1ce1bca0db8990b57385f5dc86cbe8
Shavkat Nasirov <shavkat.dev@gmail.com>
2024-01-15 13:57:05 +0900

    implement search keyword enter event

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { featheWeather } from "./api/fetchWeather";
+import { fetchWeather } from "./api/fetchWeather";
 import "./App.css";
 
 const App = () => {
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({})
+
+    const search = async (e) => {
+        console.log(e.key);
+        if(e.key === 'Enter') {
+            const data = await fetchWeather(query);
+            setWeather(data);
+            console.log(data);
+            setQuery('');
+        }
+    }
+
     return (
-        <div>
-        <h1>App</h1>
+        <div className="main-container">
+            <input type="text" className="search" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
+
         </div>
     );
 }
