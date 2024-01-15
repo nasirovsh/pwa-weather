4bebb7bc610e29d4f56ebcb6fd646a0e26be4799
Shavkat Nasirov <shavkat.dev@gmail.com>
2024-01-15 13:56:39 +0900

    fix typo

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+// const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const API_KEY = 'e13a2ea2e9ca7853f40a467d71f469f2';
 
-export const featheWeather = async (query) => {
+export const fetchWeather = async (query) => {
   const { data } = await axios.get(URL, {
     params: {
         q: query,
